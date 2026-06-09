72480aeb7fc1c99f39bab4e21a4c31f7c66e0b9d
WesyPlayz <rharding1022@gmail.com>
2026-06-09 05:51:55 -0700

    UPDATE v0.2
    Added code

--- Battleship/Battleship.AI.cs
+++ Battleship/Battleship.AI.cs
@@ -5,5 +5,15 @@
 /// </summary>
 public sealed class BoardBot : Board
 {
+    #region PRIVATE INSTANCE FIELDS
 
+    private readonly Random Generator = new ();
+
+    #endregion
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name = "name"></param>
+    public BoardBot ( string name ) : base ( name ) {}
 }
--- Battleship/Battleship.cs
+++ Battleship/Battleship.cs
@@ -5,7 +5,28 @@
 /// </summary>
 public static class Manager
 {
+    #region INTERNAL STATIC FIELDS
 
+    /// <summary>
+    /// 
+    /// </summary>
+    internal static Dictionary < string, int > Types = [];
+
+    internal static int Rows;
+    internal static int Columns;
+
+    #endregion
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name = "rows"></param>
+    /// <param name = "columns"></param>
+    public static void Board_Size ( int rows, int columns ) 
+    {
+        Rows = rows;
+        Columns = columns;
+    }
 }
 
 /// <summary>
@@ -13,13 +34,54 @@ public static class Manager
 /// </summary>
 public class Board
 {
+    #region INTERNAL INSTANCE FIELDS
 
+    /// <summary>
+    /// 
+    /// </summary>
+    internal readonly ( Boat? BOAT, bool STATUS )[][] Grid;
+
+    internal readonly string Name;
+
+    #endregion
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name = "name"></param>
+    public Board ( string name ) 
+    {
+        this.Grid = new ( Boat? BOAT, bool STATUS )[ Manager.Rows ][];
+        this.Name = name;
+
+        for ( int idx = 0; idx < Manager.Rows; idx++ ) this.Grid[ idx ] = new ( Boat? BOAT, bool STATUS)[ Manager.Columns ];
+    }
 }
 
 /// <summary>
 /// 
 /// </summary>
-internal class Boat
+/// <param name = "name"></param>
+internal class Boat ( string name, int length )
 {
+    internal readonly string Name = name;
 
+    protected bool Rotation;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    internal int Length { get; private set; } = length;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name = "name"></param>
+    /// <returns></returns>
+    internal static Boat? Create ( string name ) 
+    {
+        if ( !Manager.Types.TryGetValue( name, out int dLength ) ) return null;
+
+        return new ( name, dLength );
+    }
 }
